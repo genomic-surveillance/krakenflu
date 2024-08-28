ccccf6434ff3fade6ba504bbd284d2deadb87e96
Frank Schwach <fs5@sanger.ac.uk>
2024-08-28 10:15:43 +0100

    updated CLI cmd

--- kraken_flu/cmd.py
+++ kraken_flu/cmd.py
@@ -1,7 +1,7 @@
 import argparse
 import os
-import shutil
-import logging
+import tempfile 
+
 from kraken_flu.src.kraken_db_builder import KrakenDbBuilder
 
 # get the version number from a file that is created by setuptools_scm 
@@ -17,7 +17,7 @@ except ImportError:
 # The script uses absolute imports that will only work after being installed by pip.
 # To run the script during development, run as follows from top level directory of the repo:
 # python -m kraken_flu.cmd { OPTIONS }
-
+            
 def args_parser():
     """
     Command line argument parser
@@ -39,12 +39,13 @@ def args_parser():
         help='path to the NCBI taxonomy directory that contains files nodes.dmp and names.dmp')
 
     parser.add_argument(
-        '--fasta_path','-l',
+        '--fasta_path','-f',
         action = 'store',
         required = True,
-        metavar = 'DIR', 
+        metavar = 'FILE', 
         type = str,
-        help='path to the sequences FASTA path')
+        nargs='+',
+        help='one or more filepath for sequence FASTA file(s)')
 
     parser.add_argument(
         '--out_dir','-o',
@@ -56,9 +57,24 @@ def args_parser():
     )
     
     parser.add_argument(
-        '--filter','-f',
+        '--db_file','-d',
+        type = str,
+        action = 'store',
+        required = False,
+        metavar = 'FILE', 
+        help = 'File path for the sqlite backend DB file created by this tool. If not provided, a tmp file will be used.'
+    )
+
+    parser.add_argument(
+        '--keep_db_file',
         action = 'store_true',
-        help = 'when used, the FASTA file will be filtered to remove incomplete influenza genomes'        
+        help = 'when used, the sqlite backend DB file is not deleted at the end of the process and can be further investigated using sqlite'        
+    )
+    
+    parser.add_argument(
+        '--filter_flu',
+        action = 'store_true',
+        help = 'apply the influenza filters: remove incomplete genomes (also see filter_except) and sequences with non-standard isolate names'        
     )
     
     parser.add_argument(
@@ -80,18 +96,28 @@ def args_parser():
 def main():
     
     args = args_parser().parse_args()
-    kdb = KrakenDbBuilder(
-        taxonomy_path= args.taxonomy_path, 
-        fasta_file_path= args.fasta_path,
-    )    
-    kdb.create_db_ready_dir( 
-        path = args.out_dir,
-        force= True, 
-        fasta_file_name= 'library.fna', 
-        filter_incomplete_flu= args.filter,
-        filter_except_patterns = args.filter_except,
-        drop_unparsed_flu = args.drop_unparsed_flu
-    )
+    db_path = args.db_file or tempfile.NamedTemporaryFile(delete= not args.keep_db_file)
+    
+    kdb = KrakenDbBuilder(db_path= db_path)
+    kdb.load_taxonomy_files(args.taxonomy_path)
+    
+    # Load the bulk of the reference genomes from FASTA files.
+    # These are loaded into the "sequences" table of the sqlite DB without a category value, 
+    for fasta_path in args.fasta_path:
+        kdb.load_fasta_file(fasta_path)
+        
+    if args.filter_flu:
+        kdb.filter_unnamed_unsegmented_flu()
+        if args.filter_except:
+            filter_except_list = args.filter_except
+        else:
+            filter_except_list = []
+        kdb.filter_incomplete_flu(filter_except_patterns= filter_except_list)
+        
+    kdb.create_segmented_flu_taxonomy_nodes()
+    kdb.assign_flu_taxonomy_nodes()
+    
+    kdb.create_db_ready_dir(path = args.out_dir)
 
 if __name__ == "__main__":
     exit(main())
