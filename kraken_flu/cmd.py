53e31fbbf8fc03ddb846449df5bdd7e33f75013c
“bd8” <bd8@sanger.ac.uk>
2024-10-03 11:33:08 +0100

    add switches for multiref repair, add logging

--- kraken_flu/cmd.py
+++ kraken_flu/cmd.py
@@ -1,6 +1,7 @@
 import argparse
 import os
-import tempfile 
+import tempfile
+import logging
 
 from kraken_flu.src.kraken_db_builder import KrakenDbBuilder
 
@@ -92,6 +93,20 @@ def args_parser():
         help = 'one or more strings/patterns that are used to exclude genomes from the Influenza "complete genome" filter (if used)'
     )
 
+    parser.add_argument(
+        '--repair_subterminal_multirefs',
+        action='store_true',
+        required= False,
+        help = 'Repair cases where a given path in the taxonomy contains sequences at subterminal nodes.'
+    )
+
+    parser.add_argument(
+        '--repair_all_multirefs',
+        action='store_true',
+        required= False,
+        help = 'Repair cases where a given path in the taxonomy contains sequences at any non-leaf nodes.'
+    )
+
     return parser
 
 def main():
@@ -104,12 +119,12 @@ def main():
         taxonomy_dir= args.taxonomy_path, 
         no_acc2taxid= args.no_acc2taxid
     )
-    
+
     # Load the bulk of the reference genomes from FASTA files.
     # These are loaded into the "sequences" table of the sqlite DB without a category value, 
     for fasta_path in args.fasta_path:
         kdb.load_fasta_file(fasta_path)
-        
+
     if args.filter_flu:
         kdb.filter_unnamed_unsegmented_flu()
         if args.filter_except:
@@ -117,13 +132,19 @@ def main():
         else:
             filter_except_list = []
         kdb.filter_incomplete_flu(filter_except_patterns= filter_except_list)
-        
+
     kdb.create_segmented_flu_taxonomy_nodes()
     kdb.assign_flu_taxonomy_nodes()
 
-
     multiref_paths, seen, multiref_data = kdb.find_multiref_paths(root_taxid = 10239)
-    kdb.repair_multiref_paths(multiref_paths, seen)
+    if not args.repair_subterminal_multirefs and not args.repair_all_multirefs:
+        logging.info("Paths with sequences attached to non-leaf nodes will not be fixed.")
+    if args.repair_subterminal_multirefs and not args.repair_all_multirefs:
+        logging.info("Repairing only subterminal multiref cases.")
+        kdb.repair_multiref_paths(multiref_paths, seen, "subterminal")
+    if args.repair_all_multirefs:
+        logging.info("Repairing all multiref cases.")
+        kdb.repair_multiref_paths(multiref_paths, seen, "all")
 
     kdb.create_db_ready_dir(path = args.out_dir)
 
